1434e5f1f04f459ab8a35c01decce45546a90348
Coda Bool <codabool@pm.me>
2023-10-27 19:07:35 -0400

    use link to purchased content section

--- pages/module.js
+++ pages/module.js
@@ -37,7 +37,7 @@ export default function Module() {
       
       <h1 className="display-1 my-5">🔐 Where to use your key</h1>
 
-      <h4>If you have your key you can now activate it at <a href="https://foundryvtt.com/article/premium-content">foundry.com</a> by going to your account's "Purchased Content" section. Entering your key from itch.io and activating it. This will permanently link access to the module to your Foundry account. You can now install the module at the setup screen of your Foundry instance.</h4>
+      <h4>If you have your key you can now activate it at <a href="https://foundryvtt.com/article/premium-content">foundry.com</a> by going to your account's <a href="https://foundryvtt.com/me/purchases">Purchased Content</a> section. Entering your key from itch.io and activating it. This will permanently link access to the module to your Foundry account. You can now install the module at the setup screen of your Foundry instance.</h4>
 
       <img className="w-100 my-3"  src="https://foundryvtt.s3.us-west-2.amazonaws.com/website-media-dev/user_671/screen/premium-content-activation-2021-02-25.webp" />
 
