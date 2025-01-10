e228d8538fdae6df55e86940166abe4576db39c5
Coda Bool <CodaBool@pm.me>
2025-01-09 23:15:19 -0500

    template out blog 36

--- app/(main)/about/page.jsx
+++ app/(main)/about/page.jsx
@@ -12,7 +12,7 @@ export default function About() {
         </div>
         <div className="w-full mb-8 md:w-1/2">
           <h1 className="m-2 ml-10 text-4xl">Hello, <div className="inline-block animate-bounce" >👋</div></h1>
-          <p className="m-5 text-xl">&emsp;I am a DevOps engineer, but I also do full-stack development outside of work. I mainly work with <strong>AWS</strong> and for my full stack projects I enjoy Next.js which is a React framework that uses serverless technology for its backend. I like to use PostgreSQL + Prisma for my database solutions. But recently I've also been using Go for backend projects. You can see more on my projects and blog pages. Thanks for visiting. </p>
+          <p className="m-5 text-xl">&emsp;I am a DevOps engineer, but I also do full-stack development outside of work. I mainly work with <strong>AWS</strong>. I like to use PostgreSQL + Prisma for my database solutions. For my full stack projects I enjoy Next.js. But recently I've also been using Go for backend projects. You can see more on my projects and blog pages. <b style={{ fontWeight: 550 }}>Thanks for visiting</b>. </p>
         </div>
       </div>
       <br />
